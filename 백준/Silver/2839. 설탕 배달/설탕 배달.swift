7bc9e806fb42a71f389df15bfa3624d25c1761f4
GRAPE <pjsk419@naver.com>
2024-03-13 20:19:25 +0900

    [Silver IV] Title: 설탕 배달, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.swift
@@ -0,0 +1,18 @@
+var sugar = Int(String(readLine()!))!
+var bag = 0
+
+while sugar >= 0 {
+    if sugar % 5 == 0 {
+        bag += sugar / 5
+        print(bag)
+        break
+    }
+    sugar -= 3
+    bag += 1
+    
+    if sugar < 0 {
+        print(-1)
+        break
+    }
+}
+
